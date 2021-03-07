587acff89f05152a948247ad7205494d926481cf
Sammy-Nyakabau <sammy@sammynyakabau.com>
2021-03-07 15:16:36 +0530

    ✨ New Feature: add particles background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
@@ -9,6 +10,34 @@ const StyledMainContainer = styled.main`
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
+    <Particles
+      params={{
+        particles: {
+          number: {
+            value: 50,
+          },
+          size: {
+            value: 3,
+          },
+          color: {
+            value: '#eebbc3',
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'repulse',
+            },
+          },
+        },
+      }}
+      style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+      }}
+    />
     <StyledMainContainer className="fillHeight">
       <Hero />
       <About />
